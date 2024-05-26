951f1088f4f4e9cf1aef20430561ee329b90a2c4
Matija Pajenk <matija.pajenk@student.um.si>
2024-05-26 15:42:54 +0200

    refill database

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ let path = require('path')
 let cookieParser = require('cookie-parser')
 let logger = require('morgan')
 require("dotenv").config()
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const http = require("http")
+const {Server} = require("socket.io")
+const cors = require("cors")
 
 // MongoDB
 let mongoose = require('mongoose').set('strictQuery', true)
 const mongoDB = process.env.DB_URL
 
-console.log("My connection string: " + mongoDB);
 mongoose.connect(mongoDB)
 mongoose.Promise = global.Promise
 let db = mongoose.connection
@@ -20,8 +19,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
-const adminRouter = require('./routes/adminRouter');
-const eventsRouter = require('./routes/events');
+const adminRouter = require('./routes/adminRouter')
+const eventsRouter = require('./routes/events')
 
 const footballTeamRouter = require('./routes/football/teamRoutes')
 const footballStadiumRouter = require('./routes/football/stadiumRoutes')
@@ -36,62 +35,62 @@ const handballMatchRouter = require('./routes/handball/matchRoutes')
 let app = express()
 
 // cors rules
-const allowedOrigins = ['*', 'http://localhost:3000', "http://localhost:5173"];
+const allowedOrigins = ['*', 'http://localhost:3000', "http://localhost:5173"]
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // Allow credentials (cookies, authorization headers, etc.)
-}));
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}))
 
 // Web Socket
-const eventModel = require('./models/eventModel');
-const jwt = require('jsonwebtoken');
-const server = http.createServer(app);
+const eventModel = require('./models/eventModel')
+const jwt = require('jsonwebtoken')
+const server = http.createServer(app)
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
-  }
-});
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    }
+})
 
 async function createEvent(socket, data) {
-  try {
-    const decodedToken = jwt.verify(data.token, process.env.JWT_SECRET);
-    data.host = decodedToken.id;
-  } catch (err) {
-    if (err.name === 'TokenExpiredError') {
-      socket.emit("error", {message: "JWT expired."}); 
-      return;
-    } else {
-      socket.emit("new-event", {message: "JWT verification failed."}); 
-      return;
+    try {
+        const decodedToken = jwt.verify(data.token, process.env.JWT_SECRET)
+        data.host = decodedToken.id
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            socket.emit("error", {message: "JWT expired."})
+            return
+        } else {
+            socket.emit("new-event", {message: "JWT verification failed."})
+            return
+        }
     }
-  }
-  let newEvent = new eventModel(data);
-  newEvent.save(function(err, event) {
-    if (err) {
-      socket.send("error", {message: "failed to create event"});
-    }
-    //socket.broadcast.emit("new-event", data);
-    socket.emit("new-event", event); 
-  })
-}  
+    let newEvent = new eventModel(data)
+    newEvent.save(function (err, event) {
+        if (err) {
+            socket.send("error", {message: "failed to create event"})
+        }
+        //socket.broadcast.emit("new-event", data);
+        socket.emit("new-event", event)
+    })
+}
 
 io.on("connection", (socket) => {
-  socket.on("create-event", (data) => {
-    createEvent(socket, data);
-  })
+    socket.on("create-event", (data) => {
+        createEvent(socket, data)
+    })
 })
 
 server.listen(3001, () => {
-  console.log("Server is running!");
+    console.log("Server is running!")
 })
 
 // view engine setup
@@ -100,7 +99,7 @@ app.set('view engine', 'hbs')
 
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -121,18 +120,18 @@ app.use('/handballMatch', handballMatchRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
+    next(createError(404))
 })
 
 // error handler
 app.use(function (err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
 })
 
 module.exports = app
