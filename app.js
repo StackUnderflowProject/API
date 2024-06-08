cf08611dbaca75a46da4fc2c86df7d61ebe4df03
Tadej <tadej.tersek03@gmail.com>
2024-06-08 13:40:26 +0200

    SCRUM-148 updated path to relative

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
-app.use(express.static('/home/MatijaPajenk/public'))
+app.use(express.static('./public'))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
